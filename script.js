3b75f4f4002a4ee5384f2090669e07d1a795fe07
AndriySydorchuk <programmers130822@gmail.com>
2023-08-17 13:44:29 +0300

    Create grid items, style them

---
+++ script.js
@@ -0,0 +1,14 @@
+const userInput = 16;
+const grid = document.querySelector(".grid");
+
+function createGrid() {
+  for (let i = 0; i < userInput * userInput; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid__item");
+    gridItem.style.width = grid.offsetWidth / userInput + "px";
+    gridItem.style.height = gridItem.style.width;
+    grid.appendChild(gridItem);
+  }
+}
+
+createGrid();
